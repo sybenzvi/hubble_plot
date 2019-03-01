a05d5f6d836fad69be8608713f363bf9b2fd9236
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2019-03-01 15:50:22 -0800

    Enable plotting of Hubble diagram.

--- index.php
+++ index.php
@@ -14,16 +14,23 @@ if (is_null($_SESSION["redshifts"])) {
  */
 $post_commands = array_keys($_POST);
 if (!(empty($_POST))) {
-  $wavelength = $_POST["wavelength"];
-  $z = ($wavelength - 6563.0) / 6563.0;
-  $S = $_POST["brightS"];
-  $E = $_POST["brightE"];
-  $d = sqrt($E / (4 * 3.14159265359 * $S));
-  /*
-   * Push values into session variables.
-   */
-  array_push($_SESSION["redshifts"], $z);
-  array_push($_SESSION["distances"], $d);
+  if (array_key_exists("restart", $_POST)) {
+    session_unset();
+  }
+  else {
+    $wavelength = $_POST["wavelength"];
+    $z = ($wavelength - 6563.0) / 6563.0;
+    $S = $_POST["brightS"];
+    $E = $_POST["brightE"];
+    $v = 299792.458 * $z;
+    $d = sqrt($E / (4 * 3.14159265359 * $S)) * 3240.78;
+    /*
+     * Push values into session variables.
+     */
+    array_push($_SESSION["redshifts"], $z);
+    array_push($_SESSION["distances"], $d);
+    array_push($_SESSION["velocities"], $v);
+  }
 }
 ?>
 
@@ -34,6 +41,42 @@ if (!(empty($_POST))) {
     <style type="text/css" media="screen">
       @import "css/styles.css";
     </style>
+<?php
+  /*
+   * If we have redshift measurements, plot them.
+   */
+  if (!empty($_SESSION["redshifts"])) {
+    echo "<script>";
+    echo "window.onload = function() {";
+    echo "    var chart = new CanvasJS.Chart(\"chartContainer\", {";
+    echo "	animationEnabled: false,";
+    echo "	zoomEnabled: true,";
+    echo "	title:{";
+    echo "		text: \"Hubble Diagram\"";
+    echo "	},";
+    echo "	axisX: {";
+    echo "		title:\"distance [Mpc]\",";
+    echo "	},";
+    echo "	axisY:{";
+    echo "		title: \"velocity [km/s]\",";
+    echo "	},";
+    echo "	data: [{";
+    echo "		type: \"scatter\",";
+    echo "		dataPoints: [";
+    for ($i=0; $i<count($_SESSION["distances"]); $i++) {
+      $x = $_SESSION["distances"][$i];
+      $y = $_SESSION["velocities"][$i];
+      echo "    { x: " . $x . ", y: " . $y . "},";
+    }
+    echo "		]";
+    echo "	}]";
+    echo "});";
+    echo "chart.render();";
+    echo "";
+    echo "}";
+    echo "</script>";
+  }
+?>
   </head>
   <body>
   <div class="wrapper">
@@ -42,18 +85,11 @@ if (!(empty($_POST))) {
 
   <h1>Interactive Hubble Diagram</h1>
 
-  <h2>Instructions</h2>
-  
-  <p>In this exercise you will use the H&alpha; emission from galaxies measured
-  by the Sloan Digital Sky Survey to estimate the recession velocities of
-  objects in the nearby universe.</p>
-
-  <p>Using these data, you can then construct a Hubble Diagram &#8212; a plot
-  of recession velocity versus distance &#8212; and estimate Hubble's
-  constant.</p>
-
 <?php
 if (empty($_SESSION["redshifts"])) {
+  echo "<h2>Instructions</h2>";
+  echo "<p>In this exercise you will use the H&alpha; emission from galaxies measured by the Sloan Digital Sky Survey to estimate the recession velocities of objects in the nearby universe.</p>";
+  echo "<p>Using these data, you can then construct a Hubble Diagram &#8212; a plot of recession velocity versus distance &#8212; and estimate Hubble's constant.</p>";
   echo "<h2>Get Started</h2>";
   echo "<p>To begin, click the button below. You will be taken to a page
   showing the image and spectrum of a galaxy measured by SDSS. Estimate the
@@ -64,8 +100,23 @@ if (empty($_SESSION["redshifts"])) {
   echo "</form>";
 }
 else {
-  echo "<p><em>z</em> = " . $z . "</p>";
-  echo "<p><em>d</em> = " . $d . "</p>";
+  echo "<h2>Results</h2>";
+  echo "<div id=\"chartContainer\" style=\"height:400px; width: 50%;\"></div>";
+  echo "<script src=\"https://canvasjs.com/assets/script/canvasjs.min.js\"></script>";
+  /*
+   * Loop through session redshifts and distances.
+   */
+  /*
+  for ($i=0; $i<count($_SESSION["distances"]); $i++) {
+    echo $_SESSION["distances"][$i] . ", " . $_SESSION["velocities"][$i] . "<br/>";
+  }
+  */
+  echo "<p><a href=\"chart.php\">Run another spectrum!</a></p>";
+  echo "<h2>Restart!</h2>";
+  echo "<p>To restart the entire calculation from scratch, click below.</p>";
+  echo "<form name=\"Restart\" method=\"post\" action=\"index.php\">";
+  echo "<input type=\"submit\" name=\"restart\" value=\"Restart!\">";
+  echo "</form>";
 }
 ?>
 
